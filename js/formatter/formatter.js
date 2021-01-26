6c7db9497e4ee978963fa49233d78b16f81a483b
Mukul Chaundhyan <mukul.choundyan@gmail.com>
2021-01-26 10:36:05 +0530

    Added XML formatter and dynamic drop down selection on the basis of query param in URL.

--- js/formatter/formatter.js
+++ js/formatter/formatter.js
@@ -3,7 +3,7 @@ function format() {
     if (whichFormatter == 'jsonFormatter') {
         jsonFormatter()
     } else if (whichFormatter == 'xmlFormatter') {
-
+        xmlFormatter()
     } else if (whichFormatter == 'option1Formatter') {
 
     } else if (whichFormatter == 'option2Formatter') {
@@ -20,7 +20,16 @@ function jsonFormatter() {
 }
 
 function xmlFormatter() {
-
+    var formatted = '', indent= '';
+    tab = '\t';
+    var xml = document.getElementById("cMessage").value.trim();
+    xml.split(/>\s*</).forEach(function(node) {
+        if (node.match( /^\/\w/ )) indent = indent.substring(tab.length); // decrease indent by one 'tab'
+        formatted += indent + '<' + node + '>\r\n';
+        if (node.match( /^<?\w[^>]*[^\/]$/ )) indent += tab;              // increase indent
+    });
+    document.getElementById("cMessage").value = formatted.substring(1, formatted.length-3);
+    document.getElementById("cMessage").select();
 }
 
 function clearText() {
