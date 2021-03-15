74808cfb64af1117698d2f3f9d17ac909dce47f6
Mukul Chaundhyan <mukul.choundyan@gmail.com>
2021-03-15 14:38:39 +0530

    Updated email address and few formatting changes. Also added WIP utility features.

--- js/converter/converter.js
+++ js/converter/converter.js
@@ -1,4 +1,4 @@
-function clearText() {
+function clearTimeText() {
     document.getElementById("nanoSeconds").value = "";
     document.getElementById("milliSeconds").value = "";
     document.getElementById("seconds").value = "";
@@ -7,7 +7,11 @@ function clearText() {
     document.getElementById("day").value = "";
 }
 
-function convert(textBox) {
+function clearConverterText() {
+	document.getElementById("cMessage").value = "";
+}
+
+function convertTime(textBox) {
 	toSecond(textBox);
 	toNanoSeconds(textBox);
 	toMilliSeconds(textBox);
@@ -104,4 +108,82 @@ function toDays(textBox) {
 	} else if (textBox.id == "milliSeconds") {
 		document.getElementById("day").value = value * 0.000000011575;
 	}
+}
+
+function convert() {
+    var whichConverter = document.getElementById("converter-id").value;
+    if (whichConverter == 'xmlToJSON') {
+        xmlToJSONConverter(new DOMParser().parseFromString(document.getElementById("cMessage").value.trim(), 'text/xml'))
+    } else if (whichConverter == 'jsonToXml') {
+        jsonToXmlConverter()
+    } else {
+        document.getElementById("cMessage").value = "Invalid Converter selected.";
+    }
+}
+
+function xmlToJSONConverter(xml) {
+	// Create the return object
+      var obj = {};
+
+      if (xml.nodeType == 1) {
+        // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+          obj["@attributes"] = {};
+          for (var j = 0; j < xml.attributes.length; j++) {
+            var attribute = xml.attributes.item(j);
+            obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+          }
+        }
+      } else if (xml.nodeType == 3) {
+        // text
+        obj = xml.nodeValue;
+      }
+
+      // do children
+      // If all text nodes inside, get concatenated text from them.
+      var textNodes = [].slice.call(xml.childNodes).filter(function(node) {
+        return node.nodeType === 3;
+      });
+      if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+        obj = [].slice.call(xml.childNodes).reduce(function(text, node) {
+          return text + node.nodeValue;
+        }, "");
+      } else if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+          var item = xml.childNodes.item(i);
+          var nodeName = item.nodeName;
+          if (typeof obj[nodeName] == "undefined") {
+            obj[nodeName] = xmlToJSONConverter(item);
+          } else {
+            if (typeof obj[nodeName].push == "undefined") {
+              var old = obj[nodeName];
+              obj[nodeName] = [];
+              obj[nodeName].push(old);
+            }
+            obj[nodeName].push(xmlToJSONConverter(item));
+          }
+        }
+      }
+      document.getElementById("cMessage").value = JSON.stringify(obj, undefined, 4);
+}
+
+function jsonToXmlConverter() {
+var Parser = require("fast-xml-parser").j2xParser;
+//default options need not to set
+var defaultOptions = {
+    attributeNamePrefix : "@_",
+    attrNodeName: "@", //default is false
+    textNodeName : "#text",
+    ignoreAttributes : true,
+    cdataTagName: "__cdata", //default is false
+    cdataPositionChar: "\\c",
+    format: false,
+    indentBy: "  ",
+    supressEmptyNode: false,
+    tagValueProcessor: a=> he.encode(a, { useNamedReferences: true}),// default is a=>a
+    attrValueProcessor: a=> he.encode(a, {isAttributeValue: isAttribute, useNamedReferences: true})// default is a=>a
+};
+var parser = new Parser(defaultOptions);
+var xml = parser.parse(document.getElementById("cMessage").value.trim());
 }
